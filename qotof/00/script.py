916288c8a071f2f298203bd5562cabcf6e0619f0
Haitham El-Ghareeb <helghareeb@gmail.com>
2017-12-15 14:46:37 +0200

    enhanced qotof

--- qotof/00/script.py
+++
@@ -1,7 +0,0 @@
-import os
-i=0
-for filename in os.listdir('.'):
-    if filename.endswith(".jpg"): 
-        # print(os.path.join(directory, filename))
-        os.rename(filename, str(i)+'.jpeg')
-        i+=1
